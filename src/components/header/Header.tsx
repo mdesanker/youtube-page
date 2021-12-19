c809a91e60df3aff41822a48be50e69a35b5c93e
Michael Desanker <mdesanker@gmail.com>
2021-12-19 10:08:10 -0500

    Add logo to header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import Logo from "./Logo";
 import Navicon from "./Navicon";
 
 const Header: React.FC = () => {
   return (
     <HeaderWrapper>
       <Navicon />
+      <Logo />
     </HeaderWrapper>
   );
 };
---
+++ src/components/header/Logo.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+import logo from "../../assets/logo.png";
+
+const Logo: React.FC = () => {
+  return (
+    <Wrapper>
+      <Icon src={logo} alt="logo" />
+      <Name>YouTube</Name>
+    </Wrapper>
+  );
+};
+
+const Name = styled.p`
+  color: #fff;
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+const Icon = styled.img`
+  height: 20px;
+  background-color: transparent;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 5px;
+  height: 56px;
+  padding: 18px 14px 18px 16px;
+`;
+
+export default Logo;
