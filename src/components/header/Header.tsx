aa445f9b37a234d9286d9450a0c8e44ffdd10bd0
Michael Desanker <mdesanker@gmail.com>
2021-12-19 06:15:12 -0500

    Clear out starter files

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header: React.FC = () => {
+  return <HeaderWrapper></HeaderWrapper>;
+};
+
+const HeaderWrapper = styled.header``;
+
+export default Header;
