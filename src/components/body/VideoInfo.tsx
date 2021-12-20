ac309a639949d4543bc2dacab11bfe9a0aeb3859
Michael Desanker <mdesanker@gmail.com>
2021-12-20 13:09:49 -0500

    Add title and video stats

--- src/components/body/VideoInfo.tsx
+++ src/components/body/VideoInfo.tsx
@@ -2,14 +2,45 @@ import React from "react";
 import styled from "styled-components";
 
 const VideoInfo: React.FC = () => {
-  return <Wrapper>Text</Wrapper>;
+  return (
+    <>
+      <TitleWrapper>
+        <Title>Saitama vs Genos Fight | One Punch Main (60FPS)</Title>
+        <SubTitle>
+          <Views>27,218,848 views &#183; Oct 19, 2016</Views>
+          <BtnContainer></BtnContainer>
+        </SubTitle>
+      </TitleWrapper>
+    </>
+  );
 };
 
-const Wrapper = styled.div`
+const BtnContainer = styled.div``;
+
+const Views = styled.p`
+  font-size: 14px;
+  color: #606060;
+  line-height: 16px;
+`;
+
+const SubTitle = styled.div`
   width: 100%;
-  max-width: 1000px;
-  margin: 0 auto;
-  border: 1px solid red;
+  height: 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 18px;
+  color: #030303;
+  font-weight: 400;
+`;
+
+const TitleWrapper = styled.div`
+  width: 100%;
+  padding: 20px 0 8px;
+  border-bottom: 1px solid lightgray;
 `;
 
 export default VideoInfo;
--- src/components/body/YoutubeEmbed.tsx
+++ src/components/body/YoutubeEmbed.tsx
@@ -22,7 +22,6 @@ const YoutubeEmbed: React.FC<Props> = ({ embedId }) => {
 
 const Wrapper = styled.div`
   width: 100%;
-  max-width: 1000px;
   justify-content: center;
   position: relative;
   overflow: hidden;
@@ -40,11 +39,6 @@ const Wrapper = styled.div`
     top: 0;
     height: 100%;
     width: 100%;
-  }
-
-  @media (max-width: 1000px) {
-    width: 100%;
-    margin: 0 auto;
   }
 `;
 
