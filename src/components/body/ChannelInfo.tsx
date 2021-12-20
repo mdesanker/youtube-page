c1a89f3199c94a4d1505060cecefd68a2fb5793a
Michael Desanker <mdesanker@gmail.com>
2021-12-20 17:51:18 -0500

    Add channel info text

--- src/components/body/ChannelInfo.tsx
+++ src/components/body/ChannelInfo.tsx
@@ -6,10 +6,50 @@ const ChannelInfo: React.FC = () => {
   return (
     <Wrapper>
       <ProfileImg className="far fa-user" />
+      <ChannelDetails>
+        <h3>60FpsStation</h3>
+        <h5>22.2K subscribers</h5>
+        <p>I do not own any rights to the anime or its music.</p>
+        <p>
+          Copyright Disclaimer Under Section 107 of the Copyright Act 1976,
+          allowance is made for "fair use"
+        </p>
+        <button>Show more</button>
+      </ChannelDetails>
       <SubscribeBtn text="subscribe" />
     </Wrapper>
   );
 };
+
+const ChannelDetails = styled.div`
+  flex-grow: 1;
+
+  & h3 {
+    font-size: 14px;
+    padding-bottom: 5px;
+  }
+
+  & h5 {
+    font-weight: normal;
+    color: #606060;
+    font-size: 12px;
+  }
+
+  & p {
+    padding-top: 20px;
+    color: #030303;
+    font-size: 14px;
+  }
+
+  & button {
+    padding-top: 10px;
+    color: #606060;
+    font-size: 12px;
+    text-transform: uppercase;
+    background-color: transparent;
+    border: none;
+  }
+`;
 
 const ProfileImg = styled.i`
   height: 48px;
@@ -30,6 +70,7 @@ const Wrapper = styled.div`
   border-bottom: 1px solid lightgray;
 
   display: flex;
+  gap: 10px;
 `;
 
 export default ChannelInfo;
